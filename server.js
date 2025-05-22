63eac397540e7d5ba04acb2ffd674f5b25084b22
sam0l <sam0l@SPMLINK-STP>
2025-05-23 03:11:15 +0800

    sss

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const pdfParse = require('pdf-parse');
 const mammoth = require('mammoth');
 const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(fileUpload());
 app.use(express.json()); // Added to parse JSON bodies
